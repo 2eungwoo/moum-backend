152e8a0f745ae2610d1c4c08596292b2d85514be
swlee <swo98@naver.com>
2025-09-06 22:34:08 +0900

    feat: Add scheduler to run ranking sync job periodically

---
+++ src/main/java/jsl/moum/MoumApplication.java
@@ -0,0 +1,15 @@
+package jsl.moum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class MoumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoumApplication.class, args);
+    }
+
+}
---
+++ src/main/java/jsl/moum/batch/BatchScheduler.java
@@ -0,0 +1,26 @@
+package jsl.moum.batch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BatchScheduler {
+
+    private final JobLauncher jobLauncher;
+    private final Job rankingSyncJob;
+
+    // Run every hour at the start of the hour
+    @Scheduled(cron = "0 0 * * * ?")
+    public void runRankingSyncJob() throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("time", System.currentTimeMillis())
+                .toJobParameters();
+        jobLauncher.run(rankingSyncJob, jobParameters);
+    }
+}
